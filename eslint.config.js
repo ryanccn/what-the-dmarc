0cb5a20229613712a9d89baff0928d56fbc28576
Ryan Cao <hello@ryanccn.dev>
2026-09-11 08:38:00 +0000

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,12 @@ import { config } from '@ryanccn/eslint-config';
 export default config({
 	svelte: true,
 	rules: {
-		'no-undef': 'off'
+		'no-undef': 'off',
+		'unicorn/no-top-level-assignment-in-function': 'off',
+		'unicorn/single-line-block-comment-style': 'off',
+		'unicorn/name-replacements': 'off',
+		'unicorn/consistent-boolean-name': 'off',
+		'unicorn/prefer-await': 'off',
+		'unicorn/no-unreadable-for-of-expression': 'off'
 	}
 });
--- src/app.d.ts
+++ src/app.d.ts
@@ -9,5 +9,4 @@ declare global {
 	}
 }
 
-// eslint-disable-next-line unicorn/require-module-specifiers
 export {};
